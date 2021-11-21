ef007629e6a0d22acbc9da3664740425639c28ab
bel ullman <iau7fs@virginia.edu>
2021-11-21 17:14:38 -0500

    groundwork

---
+++ addPiece.php
@@ -0,0 +1,46 @@
+<html> 
+<h1> Add a piece: </h1> 
+<h3> <i> Please enter the appropriate information below </i> </h3>
+<body> 
+    <form action="insertPiece.php" method="post">
+        <p>Title: <input type="text" name="title" /></p>
+        <p>Piece ID Number: <input type="text" name="pieceID" /></p>
+        <p>Type: </p>
+            <input type="radio" id="painting" name="type" value="Painting"/>
+            <label for="painting"> Painting </label><br>
+            <input type="radio" id="statue" name="type" value="Statue"/>
+            <label for="statue"> Statue </label><br>
+            <input type="radio" id="photo" name="type" value="Photograph"/>
+            <label for="photo"> Photograph </label><br>
+            <input type="radio" id="drawing" name="type" value="Drawing"/>
+            <label for="drawing"> Drawing </label><br>
+        <p>Artist ID: <input type="number" name="artist" /></p>
+        <p>Date Created: <input type="date" name="dateCreated" /></p>
+        <p>Image URL: <input type="url" name="url" /></p>
+        <p>Height: <input type="number" name="height" /></p>
+        <p>Length: <input type="number" name="length" /></p>
+        <p>Width (if two dimensional, enter 0): <input type="number" name="width" /></p>
+        <p>Description: <input type="text" name="descrip" /></p>
+        <p>Colors: </p> 
+            <input type="checkbox" name="blue" value="Blue"/> 
+            <label for="blue"> Blue </label><br>
+            <input type="checkbox" name="red" value="Red"/> 
+            <label for="red"> Red </label><br>
+            <input type="checkbox" name="green" value="Green"/> 
+            <label for="green"> Green </label><br>
+            <input type="checkbox" name="yellow" value="Yellow"/> 
+            <label for="yellow"> Yellow </label><br>
+            <input type="checkbox" name="black" value="Black"/> 
+            <label for="black"> Black </label><br>
+            <input type="checkbox" name="white" value="White"/>
+            <label for="white"> White </label><br>
+            <input type="checkbox" name="pink" value="Pink"/> 
+            <label for="pink"> Pink </label><br>
+            <input type="checkbox" name="purple" value="Purple"/>
+            <label for="purple"> Purple </label><br>
+        <p><input type="submit" /></p>
+    </form>
+    
+</body> 
+</html>
+
---
+++ insertPiece.php
@@ -0,0 +1,95 @@
+<html> 
+    <h1> Results page: </h1>
+    <body> 
+    <!-- https://www.geeksforgeeks.org/how-to-insert-form-data-into-database-using-php/ -->
+        <?php
+        // would do all the connection stuff 
+        // require "dbutil.php";
+	    // $db = DbUtil::loginConnection();
+        session_start(); 
+		$servername = "localhost"; 
+		$username = "test"; 
+		$password = "hat"; 
+		$dbname = "artGallery2"; 
+		$conn = new mysqli($servername, $username, $password, $dbname);
+        
+        $title = $_REQUEST['title'];
+        $id = $_REQUEST['pieceID'];
+        $artist = $_REQUEST['artist'];
+        $dateCreated = $_REQUEST['dateCreated'];
+        $url = $_REQUEST['url'];
+        $height = $_REQUEST['height'];
+        $width = $_REQUEST['width'];
+        $length = $_REQUEST['length'];
+        $type = $_REQUEST['type'];
+        $description = $_REQUEST['descrip'];
+        $sqlquery1 = "INSERT INTO art_piece (pieceID, title, type, image_url, width, height, length, description) VALUES ('$id', '$title', '$type', '$url', '$width', '$height', '$length', '$description')"; 
+        $sqlquery2 = "INSERT INTO created (artistID, pieceID, dateCreated) VALUES ('$artist', '$id', '$dateCreated')"; 
+
+        if (!mysqli_query($conn, $sqlquery1)) {
+            die('Error: ' . mysqli_error($conn));
+        }
+        if (!mysqli_query($con, $sqlquery2)) {
+            die('Error: ' . mysqli_error($conn));
+        }
+        if (isset($_POST['blue'])) {
+            $blue = $_REQUEST['blue'];
+            $sqlColor = "INSERT INTO art_piece_colors (pieceID, color) VALUES ('$id', '$blue')"; 
+            if (!mysqli_query($conn, $sqlColor)) {
+            	die('Error: ' . mysqli_error($conn));
+        	}
+        } if (isset($_POST['red'])) {
+            $red = $_REQUEST['red'];
+            $sqlColor = "INSERT INTO art_piece_colors (pieceID, color) VALUES ('$id', '$red')"; 
+            if (!mysqli_query($conn, $sqlColor)) {
+            	die('Error: ' . mysqli_error($conn));
+        	}
+        } if (isset($_POST['green'])) {
+            $green = $_REQUEST['green'];
+            $sqlColor = "INSERT INTO art_piece_colors (pieceID, color) VALUES ('$id', '$green')"; 
+            if (!mysqli_query($conn, $sqlColor)) {
+            	die('Error: ' . mysqli_error($conn));
+        	}
+        } if (isset($_POST['yellow'])) {
+            $yellow = $_REQUEST['yellow'];
+            $sqlColor = "INSERT INTO art_piece_colors (pieceID, color) VALUES ('$id', '$yellow')"; 
+            if (!mysqli_query($conn, $sqlColor)) {
+            	die('Error: ' . mysqli_error($conn));
+        	}
+        } if (isset($_POST['black'])) {
+            $black = $_REQUEST['black'];
+            $sqlColor = "INSERT INTO art_piece_colors (pieceID, color) VALUES ('$id', '$black')"; 
+            if (!mysqli_query($conn, $sqlColor)) {
+            	die('Error: ' . mysqli_error($conn));
+        	}
+        } if (isset($_POST['white'])) {
+            $white = $_REQUEST['white'];
+            $sqlColor = "INSERT INTO art_piece_colors (pieceID, color) VALUES ('$id', '$white')"; 
+            if (!mysqli_query($conn, $sqlColor)) {
+            	die('Error: ' . mysqli_error($conn));
+        	}
+        } if (isset($_POST['pink'])) {
+            $pink = $_REQUEST['pink'];
+            $sqlColor = "INSERT INTO art_piece_colors (pieceID, color) VALUES ('$id', '$pink')"; 
+            if (!mysqli_query($conn, $sqlColor)) {
+            	die('Error: ' . mysqli_error($conn));
+        	}
+        } if (isset($_POST['purple'])) {
+            $purple = $_REQUEST['purple'];
+            $sqlColor = "INSERT INTO art_piece_colors (pieceID, color) VALUES ('$id', '$purple')"; 
+            if (!mysqli_query($conn, $sqlColor)) {
+            	die('Error: ' . mysqli_error($conn));
+        	}
+        }	
+        echo "<h1> Success! </h1>";
+        echo "<h2> <i> Your piece is now visible in the gallery archives! </i> </h2>";
+        mysqli_close($conn);
+        
+        
+        // $result = $conn->query($sqlquery1);
+        // $result2 = $conn->query($sqlquery2);
+
+        // $db->close();
+        ?>
+    </body> 
+</html>
