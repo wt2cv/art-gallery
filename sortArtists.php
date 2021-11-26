886617afcd6b5e3ebb4d7137c4af61255187c077
Brittany <bs6sxv@virginia.edu>
2021-11-25 22:21:57 -0500

    changed to bold

--- sortArtists.php
+++ sortArtists.php
@@ -43,11 +43,11 @@ $result = $conn->query($sqlquery);
         while ($row = mysqli_fetch_assoc($result)){
 
             echo '<div class="card text-left" style="width: 30rem; margin: 1.5rem; box-shadow: 0 2px 10px rgb(0 0 0 / 0.2); word-wrap: break-word; padding: 1.5%">';
-            echo '<div style="color: grey">'. 'Artist ID: '. '<span style="color: black">' .$row["artistID"].'</span>' . '</div>';
-            echo '<div style="color: grey">'. 'First Name: '. '<span style="color: black">' .$row["firstName"].'</span>' . '</div>';
-            echo '<div style="color: grey">'. 'Last Name: '. '<span style="color: black">' .$row["lastName"].'</span>' . '</div>';
-            echo '<div style="color: grey">'. 'Address: '. '<span style="color: black">' .$row["address"].'</span>' . '</div>';
-            echo '<div style="color: grey">'. 'Country: '. '<span style="color: black">' .$row["countryOfOrigin"].'</span>' . '</div>';
+            echo '<div style="font-weight: bold;">'. 'Artist ID: '. '<span style="font-weight: normal">' .$row["artistID"].'</span>' . '</div>';
+            echo '<div style="font-weight: bold;">'. 'First Name: '. '<span style="font-weight: normal">' .$row["firstName"].'</span>' . '</div>';
+            echo '<div style="font-weight: bold;">'. 'Last Name: '. '<span style="font-weight: normal">' .$row["lastName"].'</span>' . '</div>';
+            echo '<div style="font-weight: bold;">'. 'Address: '. '<span style="font-weight: normal">' .$row["address"].'</span>' . '</div>';
+            echo '<div style="font-weight: bold;">'. 'Country: '. '<span style="font-weight: normal">' .$row["countryOfOrigin"].'</span>' . '</div>';
             echo '</div>';
     
         }
