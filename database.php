7766e3368ca4e5d5ce059281ecc57618af4e3681
Brittany <bs6sxv@virginia.edu>
2021-11-29 07:39:58 -0800

    changed server

--- database.php
+++ database.php
@@ -1,8 +1,8 @@
 <?php
-$servername = "us-cdbr-east-04.cleardb.com";
-$username = "b6021ad705af81";
-$password = "62360869";
-$dbname = "heroku_61c36e5d3c76659";
+$servername = "mysql01.cs.virginia.edu";
+$username = "bs6sxv";
+$password = "pCdqB7ewiSGc(ZY@";
+$dbname = "bs6sxv";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error){
