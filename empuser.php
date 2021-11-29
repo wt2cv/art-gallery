40eaed745155fa02a599fe4f3ffbe6d5ede7c315
emilymxiao <94804490+emilymxiao@users.noreply.github.com>
2021-11-29 18:49:15 -0500

    Update empuser.php

--- empuser.php
+++ empuser.php
@@ -1,8 +1,8 @@
 <?php
 $servername = 'mysql01.cs.virginia.edu';
-$username = 'bs6sxv_a';
+$username = 'mx2gd_a';
 $password = 'Fall2021!!';
-$dbname = 'bs6sxv';
+$dbname = 'mx2gd';
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error){
